c0cc60848fa49aab40fe31e4365ba51b70054a10
R.S.Colley U1356831 <U1356831@unimail.hud.ac.uk>
2019-02-05 11:37:46 +0000

    third commit - determine whether a given string is a palindrome

--- Recursion/src/CuSum.java
+++ Recursion/src/CuSum.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class CuSum {
     // a simple program to calculate a cumulative sum
@@ -33,19 +34,36 @@ public class CuSum {
     }*/
 
     // recursive algorithm to calculate a factorial
-    public static int recursiveCuSum(int n) {
+/*    public static int recursiveCuSum(int n) {
         if (n == 1)
             return 1;
         else
             return (n * recursiveCuSum(n - 1));
+    }*/
+
+    // recursive algorithm to determine a palindrome
+    public static boolean isPalindrome(String s) {
+        if(s.length() == 0 || s.length() == 1)
+            return true;
+        if(s.charAt(0) == s.charAt(s.length()-1))
+            return isPalindrome(s.substring(1, s.length()-1));
+        return false;
+    }
+
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("type a word to check if its a palindrome or not");
+        String x = sc.nextLine();
+        if(isPalindrome(x))
+            System.out.println(x + " is a palindrome");
+        else
+            System.out.println(x + " is not a palindrome");
     }
 
 
 
 
-
-
-    public static void main(String args[]) throws java.io.IOException{
+   /* public static void main(String args[]) throws java.io.IOException{
 
         System.out.print("Enter a number (0 to quit): ");
         int number = Integer.parseInt(In.readLine());
@@ -57,5 +75,5 @@ public class CuSum {
             System.out.print("Enter a number (0 to quit): ");
             number = Integer.parseInt(In.readLine());
         }
-    }
+    }*/
 }
